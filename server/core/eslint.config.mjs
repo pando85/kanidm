364ed6375d5805213cf89303eada395e90d1a475
Alexander Gil Casas <pando855@gmail.com>
2026-08-14 14:01:45 +0200

    fix(ui): restore guide eslint scopes

--- server/core/eslint.config.mjs
+++ server/core/eslint.config.mjs
@@ -1,16 +1,27 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
+    ignores: ["static/external/**", "static/rive/**"],
+  },
+  pluginJs.configs.recommended,
+  {
+    files: ["static/**/*.js", "static/**/*.mjs"],
     languageOptions: {
       globals: {
         ...globals.browser,
-        Base64 : "writeable" // to feed the Base64 class into the global scope
-      }
-    }
+        Base64: "writeable", // to feed the Base64 class into the global scope
+      },
+    },
   },
-  pluginJs.configs.recommended,
+  {
+    files: ["tests/**/*.mjs", "scripts/**/*.mjs"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
 ];
